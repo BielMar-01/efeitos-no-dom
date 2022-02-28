c8c6f80ab1bb76492ee01d6bced617d858bf329f
Gabriel Marques <bielmarques922@gmail.com>
2022-02-28 18:56:56 -0300

    Event Bubble

---
+++ 06-event-bubble/js/script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('button');
+const section = document.querySelector('section');
+const html = document.documentElement;
+
+function handleClick(event) {
+  event.preventDefault();
+  section.classList.add('active');
+  html.addEventListener('click', handleOutside);
+}
+
+function handleOutside(event) {
+  section.classList.remove('active');
+}
+
+btn.addEventListener('click', handleClick);
+
+btn.click();
